3282d17fa0bff0a13f30b75a7a22280ae908414f
utpbusinesssupp-cpu <utpbusinesssupp@gmail.com>
2026-06-22 16:50:32 +0200

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,8 @@
+//////////////////////////////////////////////////////
+// SUPABASE SINGLE SOURCE OF TRUTH
+//////////////////////////////////////////////////////
+
+window.sb = window.supabase.createClient(
+  "https://duznidzlfvadjcoxynjh.supabase.co",
+  "sb_publishable_3UzY44NnUmIi795wKqr2Kg_G0LBpJp4"
+);
