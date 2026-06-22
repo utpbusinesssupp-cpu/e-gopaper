c9ff60173debb01f8744bfabe960aafb8a64705d
utpbusinesssupp-cpu <utpbusinesssupp@gmail.com>
2026-06-22 17:10:57 +0200

    Update ledger.js

--- ledger.js
+++ ledger.js
@@ -1,7 +1,4 @@
-const sb = window.supabase.createClient(
-  "https://duznidzlfvadjcoxynjh.supabase.co",
-  "sb_publishable_3UzY44NnUmIi795wKqr2Kg_G0LBpJp4"
-);
+const sb = window.sb;
 
 async function loadLedger() {
 
