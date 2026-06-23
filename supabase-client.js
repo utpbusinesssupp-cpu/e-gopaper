f1f3145ef1316f772f72f340e670f7bd30c7d975
utpbusinesssupp-cpu <utpbusinesssupp@gmail.com>
2026-06-23 12:19:34 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,10 @@
 //////////////////////////////////////////////////////
-// SUPABASE SINGLE SOURCE OF TRUTH
+// SUPABASE CLIENT
 //////////////////////////////////////////////////////
 
 window.sb = window.supabase.createClient(
   "https://duznidzlfvadjcoxynjh.supabase.co",
   "sb_publishable_3UzY44NnUmIi795wKqr2Kg_G0LBpJp4"
 );
+
+console.log("Supabase Client Loaded ✔");
